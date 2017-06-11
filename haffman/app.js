a559e25689011021323afc318451420e1b44c62b
iprahtc <ipra-8@mail.ru>
2017-06-11 22:15:45 +0300

    25% работы. Завершено построение дерева

--- haffman/app.js
+++ haffman/app.js
@@ -5,16 +5,22 @@ var arrayText = text.split('');
 var NoRepeat = unique(arrayText);
 var weight = {};
 var symbol = '';
+var max = 0;
+var arrayTrue = [];
+var arrayFalse = [];
 
-NoRepeat.reverse();
+
+//NoRepeat.reverse();
+
 //получаем вес символов
 for(var x = 0; x < NoRepeat.length; x++){
     symbol = NoRepeat[x];
     weight[symbol] = substr_count(arrayText, NoRepeat[x]);
 }
 //fs.writeFileSync('weight.txt', JSON.stringify(weight));
-HuffmanEncode(weight);
-//console.log(Object.keys(weight)[0]);
+HuffmanEncode(weight, NoRepeat);
+//min(weight);
+// console.log(weight);
 
 
 //возвращает число повторений
@@ -49,14 +55,57 @@ function unique(arr) {
     return Object.keys(obj); // или собрать ключи перебором для IE8-
 }
 
+//Вызаваем цикл записи в дерево
 function HuffmanEncode(obj) {
     var flag = 0;
-    var Buff = {};
-    for (var key in obj){
-        Buff[key] = '';
+    var tables = {};
+    console.log(obj);
+    while(Object.keys(obj).length != 1){
+        min(obj);
     }
-    while(Object.keys(weight).length != 1){
+}
 
+//построение дерева
+function min(obj){
+    var minName = "";
+    var minOne = obj[Object.keys(obj)[0]];
+    var flag = 0;
+    for (var name in obj) {
+        if (minOne > obj[name]) {
+            minOne = obj[name];
+            minName = name;
+            flag++;
+        }
+        if(flag == 0){
+            flag++;
+            minName = name;
+        }
     }
-    console.log(Buff);
+    var argOne = minName;
+    delete obj[minName];
+    arrayTrue.push(argOne);
+
+    flag = 0;
+    var minTwo = obj[Object.keys(obj)[0]];
+    for (var name in obj) {
+        if (minTwo > obj[name]) {
+            minTwo = obj[name];
+            minName = name;
+            flag++;
+        }
+        if(flag == 0){
+            flag++;
+            minName = name;
+        }
+    }
+    var argTwo = minName;
+    delete obj[argTwo];
+    arrayFalse.push(argTwo);
+
+
+    obj[argOne + argTwo] = minOne+minTwo;
+
+    console.log(arrayTrue);
+    console.log(arrayFalse);
+    console.log(obj);
 }
